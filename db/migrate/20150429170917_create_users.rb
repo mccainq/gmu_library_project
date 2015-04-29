712aafc8e547081f27b94b6bd9394ed3250c5999
Quentin McCain <qmccain@gmail.com>
2015-04-29 13:45:50 -0400

    Reservations & user model. Assocations.

--- db/migrate/20150429170917_create_users.rb
+++ db/migrate/20150429170917_create_users.rb
@@ -3,12 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :user_id
-      t.digest :password_digest
+      t.string :password_digest
       t.boolean :admin
-      t.datetime :created_at
-      t.datetime :updated_at
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
---
+++ db/migrate/20150429171443_create_reservations.rb
@@ -0,0 +1,16 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.date :reserved_on
+      t.date :due_on
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.references :user, index: true
+      t.references :book, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :reservations, :users
+    add_foreign_key :reservations, :books
+  end
+end
