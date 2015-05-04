cd7f7ebc9bd62bec581ede13674dd1e1f44d6332
Quentin McCain <qmccain@gmail.com>
2015-05-03 23:58:23 -0400

    Add author_id fk to Book

---
+++ db/migrate/20150504034920_add_author_to_books.rb
@@ -0,0 +1,6 @@
+class AddAuthorToBooks < ActiveRecord::Migration
+  def change
+    add_reference :books, :author, index: true
+    add_foreign_key :books, :authors
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429171443) do
+ActiveRecord::Schema.define(version: 20150504034920) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -38,7 +38,10 @@ ActiveRecord::Schema.define(version: 20150429171443) do
     t.integer  "total_in_library"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "author_id"
   end
+
+  add_index "books", ["author_id"], name: "index_books_on_author_id"
 
   create_table "reservations", force: :cascade do |t|
     t.date     "reserved_on"
