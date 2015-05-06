7fa9ecf54956ba0c171902b693894be99a76df9b
Quentin McCain <qmccain@gmail.com>
2015-05-06 14:00:33 -0400

    Layout changes, styling & pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :books
+  resources :books do
+    get 'page/:page', :action => :index, :on => :collection
+  end
+
   resources :users
   resources :authors
 
