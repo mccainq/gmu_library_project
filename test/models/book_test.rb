d49e7acf5008aee0e3366020c21d852e8397960f
Quentin McCain <qmccain@gmail.com>
2015-05-06 21:02:24 -0400

    Recreated Author seed data using first_or_create!

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,4 +4,12 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @book = Book.new
+  end
+
+  test 'should not create book' do
+    assert_not @book.save
+  end
 end
